75c85b9498bfd112b77a51f6540a6aae8c141473
Ashley Stojak <68790374+astojak@users.noreply.github.com>
2020-08-19 22:54:20 -0500

    Update TicTacToeGame.cpp

--- TicTacToeGame.cpp
+++ TicTacToeGame.cpp
@@ -80,9 +80,9 @@ bool gameOver(char board[][3], bool &tie) {
     }
 
     // loop through board to see if all the spots are filled to see if there is a tie
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 3; j++) {
-            if((board[i][j] != 'X') && (board[i][j] != 'O')) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if ((board[i][j] != 'X') && (board[i][j] != 'O')) {
                 return false;
             }
         }
